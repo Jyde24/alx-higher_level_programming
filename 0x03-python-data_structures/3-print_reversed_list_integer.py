2cbd2a8254152c0b2a6e90ef0bf14065cd5be333
Jyde24 <chukwunonsojide.e@gmail.com>
2023-08-15 09:45:42 +0000

    Issues fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print('{:d}'.format(num))
+    if my_list:
+        list_reversed = reversed(my_list)
+        for i in list_reversed:
+            print(f"{i:d}")
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return(None)
-    else:
-        max = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max:
-                max = my_list[i]
-        return(max)
+    if my_list:
+        result = my_list[0]
+        for i in my_list[1:]:
+            if i > result:
+                result = i
+        return (result)
